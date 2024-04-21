491fe8f92e81f1d7f408c36ebe2deec6689ac369
Naresh Bhatia <me@nareshbhatia.dev>
2024-04-21 09:09:35 -0400

    configure default font-family to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        /*
+         * Customize the `sans` to font Inter
+         * Sets `font-family: var(--font-inter)` on the `html` element
+         */
+        sans: ['var(--font-inter)'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
