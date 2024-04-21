beb9daec1850fdb0eee9839daca6c8de7fcdcf43
Naresh Bhatia <me@nareshbhatia.dev>
2024-04-21 14:27:08 -0400

    configure custom colors and set default body style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+      },
       fontFamily: {
         /*
          * Customize the `sans` to font Inter
